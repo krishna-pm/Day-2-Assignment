79d81d5a608589f3cb8d6e43e642b08e570a4ade
Krishna P M <krishna00murali@gmail.com>
2021-08-28 23:21:11 +0530

    Add files via upload
    Day 2 Assignment

---
+++ script.js
@@ -0,0 +1,33 @@
+let fooditems=[
+    {
+        name: "apple",
+        price: 140,
+        quantity: "1kg"
+    },
+    {
+        name: "yogurt",
+        price: 45,
+        quantity:"90g"
+    },
+    {
+        name: "cashews",
+        price: 500,
+        quantity: "500g"
+    },
+    {
+        name: "wheatbread",
+        price: 35,
+        quantity: "450g"
+    },
+    {
+        name: "mangodrink",
+        price: 55,
+        quantity: "1L"
+    }
+
+]
+
+for(let num=0;num<fooditems.length;num++)
+{
+console.log(fooditems[num]);
+}
